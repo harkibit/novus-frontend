11b7758b06cddf757ca84905bc594bbeb49f7a56
Hiba Abdel Karim <abdelkarim.hiba@outlook.com>
2022-06-12 00:05:19 +0300

    added a taskcard and global css varibales

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import Image from "next/image";
 import { useEffect } from "react";
+import TaskCard from "../components/TaskCard";
 import styles from "../styles/Home.module.css";
 
 // import { getUsers } from "../utils/users";
