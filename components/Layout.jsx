a4945596d1c9ab2efc184d1ca818051500f04204
Hiba Abdel Karim <abdelkarim.hiba@outlook.com>
2022-06-15 13:23:23 +0300

    responsive sidebar

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -2,20 +2,32 @@ import React from "react";
 import Sidebar from "./Sidebar";
 import styles from "../styles/Home.module.css";
 
-import { Container } from "@mui/material";
+import { Box, Toolbar } from "@mui/material";
+
+const drawerWidth = 240;
 
 export default function Layout(props) {
   return (
-    <div>
+    <Box sx={{ display: "flex" }}>
       <Sidebar
         user_name="Antonio"
         user_email="antonio@gmail.com"
         user_avatar="https://icon-library.com/images/user-icon-image/user-icon-image-13.jpg"
+        pageTitle="Hello World"
       />
 
-      <div className={styles.main_container}>
-        <Container maxWidth="lg">{props.children}</Container>
-      </div>
-    </div>
+      <Box
+        component="main"
+        sx={{
+          flexGrow: 1,
+          p: 3,
+          width: { sm: `calc(100% - ${drawerWidth}px)` },
+        }}
+      >
+        {/* instead of toolbar we'll add a navbar */}
+        <Toolbar />
+        {props.children}
+      </Box>
+    </Box>
   );
 }
--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -8,11 +8,30 @@ import { FiHome } from "react-icons/fi";
 import { TbNotebook } from "react-icons/tb";
 import { BsFillGridFill } from "react-icons/bs";
 import { GiStairsGoal } from "react-icons/gi";
+import { HiMenuAlt4 } from "react-icons/hi";
+
 import { Avatar } from "@mui/material";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import CssBaseline from "@mui/material/CssBaseline";
+import Drawer from "@mui/material/Drawer";
+import IconButton from "@mui/material/IconButton";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+
+const drawerWidth = 240;
 
 export default function Sidebar(props) {
-  const { user_name, user_email, user_avatar } = props;
-  return (
+  const { user_name, user_email, user_avatar, pageTitle } = props;
+
+  const { window } = props;
+  const [mobileOpen, setMobileOpen] = React.useState(false);
+
+  const handleDrawerToggle = () => {
+    setMobileOpen(!mobileOpen);
+  };
+
+  const drawer = (
     <div className={styles.sidebar}>
       <div>
         <Image
@@ -65,5 +84,74 @@ export default function Sidebar(props) {
         </div>
       </div>
     </div>
+  );
+
+  const container =
+    window !== undefined ? () => window().document.body : undefined;
+
+  return (
+    <>
+      <CssBaseline />
+      <AppBar
+        position="fixed"
+        sx={{
+          width: { sm: `calc(100% - ${drawerWidth}px)` },
+          ml: { sm: `${drawerWidth}px` },
+        }}
+      >
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            edge="start"
+            onClick={handleDrawerToggle}
+            sx={{ mr: 2, display: { sm: "none" } }}
+          >
+            <HiMenuAlt4 />
+          </IconButton>
+          <Typography variant="h6" noWrap component="div">
+            {pageTitle}
+          </Typography>
+        </Toolbar>
+      </AppBar>
+
+      <Box
+        component="nav"
+        sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }}
+        aria-label="mailbox folders"
+      >
+        <Drawer
+          container={container}
+          variant="temporary"
+          open={mobileOpen}
+          onClose={handleDrawerToggle}
+          ModalProps={{
+            keepMounted: true, // Better open performance on mobile.
+          }}
+          sx={{
+            display: { xs: "block", sm: "none" },
+            "& .MuiDrawer-paper": {
+              boxSizing: "border-box",
+              width: drawerWidth,
+            },
+          }}
+        >
+          {drawer}
+        </Drawer>
+        <Drawer
+          variant="permanent"
+          sx={{
+            display: { xs: "none", sm: "block" },
+            "& .MuiDrawer-paper": {
+              boxSizing: "border-box",
+              width: drawerWidth,
+            },
+          }}
+          open
+        >
+          {drawer}
+        </Drawer>
+      </Box>
+    </>
   );
 }
